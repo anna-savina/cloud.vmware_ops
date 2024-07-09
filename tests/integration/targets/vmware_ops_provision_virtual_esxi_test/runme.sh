d5ee79e2bbccbb1566c3742cc7b77565f32442fa
anna-savina <asavina@redhat.com>
2024-07-09 22:50:48 +0300

    fixes

--- tests/integration/targets/vmware_ops_provision_virtual_esxi_test/runme.sh
+++ tests/integration/targets/vmware_ops_provision_virtual_esxi_test/runme.sh
@@ -7,8 +7,8 @@ ANSIBLE_TAGS=$(awk '/ansible_tags/ {print $2}' ../../integration_config.yml)
 # Check if the ANSIBLE_TAGS variable is set
 if [[ -n "$ANSIBLE_TAGS" ]]; then
   echo "ANSIBLE_TAGS is set to: $ANSIBLE_TAGS"
-  exec ansible-playbook run.yml --tags "$ANSIBLE_TAGS"
+  ansible-playbook run.yml --tags "$ANSIBLE_TAGS"
 else
   echo "ANSIBLE_TAGS is not set for Eco vCenter. Running on simulator."
-  exec ansible-playbook mock_side_effects.yml & exec ansible-playbook run.yml --tags integration-ci
+  ansible-playbook mock_side_effects.yml & ansible-playbook run.yml --tags integration-ci
 fi
